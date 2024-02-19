84a6924b12b22acf9f966a6f0e7528551fd4558a
beardfriend <beardfriend21@gmail.com>
2024-02-19 21:01:01 +0900

    fix : css main

--- frontend/src/features/sampleApp/components/Main.jsx
+++ frontend/src/features/sampleApp/components/Main.jsx
@@ -2,7 +2,7 @@ import { Card } from "@nextui-org/react";
 import React from "react";
 
 function Main({ children }) {
-  return <Card className="min-h-160 w-100 mt-3 p-6">{children}</Card>;
+  return <Card className="tab_card">{children}</Card>;
 }
 
 export default Main;
